1c595314810bb872ad0451ff62ca1c2857da3d75
Debanga prasad jena <debangaprasad07@gmail.com>
2023-08-21 14:14:51 +0530

    second commit
    a new print statement is added

--- cal.py
+++ cal.py
@@ -4,6 +4,7 @@ def calculate(a,b):
     print('Multipliction of {} and {} is = {} '.format(a,b,(a*b)))
     print('Division of {} and {} is = {} '.format(a,b,(a//b)))
     print('Modulo of {} and {} is = {} '.format(a,b,(a%b)))
+    print('end of application')
     
     
     
@@ -13,3 +14,4 @@ if __name__ == '__main__':
     a=int(input("Enter the first number: "))
     b=int(input("Enter Second Number: "))
     calculate(a,b)
+    
